6b6f9b5585896d566590c23f6995568ed0e53814
Sobid <rustamovsobid95@gmail.com>
2023-11-06 00:28:57 +0500

    3-vazifa

--- js/vazifa3.js
+++ js/vazifa3.js
@@ -1,9 +1,24 @@
-document.addEventListener("mousemove", (e) => {
+document.querySelector("body").addEventListener("dblclick", (e) => {
+  let div = document.createElement("div");
   let x = e.clientX;
   let y = e.clientY;
-  let div = document.createElement("div");
-  // div.classList.add("sticky");
+  div.classList.add("sticky");
   div.style.left = x + "px";
   div.style.top = y + "px";
-  // document.body.append(div);
+  document.body.append(div);
+  let span = document.createElement("span");
+  span.classList.add("delete");
+  span.textContent = "x";
+  div.append(span);
+  div.addEventListener("click", (e) => {
+    let p = document.createElement("p");
+    div.append(p);
+    // p.innerHTML = "fvd";
+    p.style.width = "90%";
+    p.style.height = "100%";
+    p.setAttribute("contenteditable", true);
+  });
+  span.addEventListener("click", (e) => {
+    e.target.parentElement.remove();
+  });
 });
